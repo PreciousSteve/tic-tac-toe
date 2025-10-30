37471cf05cf03a9f72bb2bff365ea2a3ad5e6976
Precious Steve <precious.steve@kodehauz.com>
2025-10-30 19:37:13 +0100

    Update play method.

--- play.py
+++ play.py
@@ -1,6 +1,5 @@
 def play(board):
     turn = "x" if board.count("x") == board.count("o") else "o"
-    print(f"Turn: {turn.upper()}")
 
     lines = [
         (0, 1, 2),
@@ -16,26 +15,19 @@ def play(board):
     for a, b, c in lines:
         line = [board[a], board[b], board[c]]
         if line.count(turn) == 2 and line.count("") == 1:
-            move = [a, b, c][line.index("")]
-            print(f"Chosen move: {move}")
-            return move
+            return [a, b, c][line.index("")]
 
     opponent = "o" if turn == "x" else "x"
     for a, b, c in lines:
         line = [board[a], board[b], board[c]]
         if line.count(opponent) == 2 and line.count("") == 1:
-            move = [a, b, c][line.index("")]
-            print(f"Chosen move: {move}")
-            return move
+            return [a, b, c][line.index("")]
 
     if board[4] == "":
-        print("Chosen move: 4")
         return 4
 
     for i, cell in enumerate(board):
         if cell == "":
-            print(f"Chosen move: {i}")
             return i
 
-    print("No vacant squares left. Cannot make a move.")
     return None
